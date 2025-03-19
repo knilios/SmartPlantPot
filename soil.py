c8c954437c3d4a29581d48230fb9de3990f21a7d
Rattapoom Pethputong <celica2548@gmail.com>
2025-03-19 11:11:16 +0700

    extract constants

--- soil.py
+++ soil.py
@@ -3,7 +3,6 @@ from umqtt.robust import MQTTClient
 import asyncio
 import json
 import machine
-import math
 import network
 import time
 
@@ -11,43 +10,43 @@ import time
 class WifiManager:
     MAX_WAIT_TIME = 30
     RECONNECT_INTERVAL = 0.5
-    
+
     def __init__(self):
         self.__wlan = network.WLAN(network.STA_IF)
         self.__led = machine.Pin(2, machine.Pin.OUT)
         self.__led.value(1)
-    
+
     def connect(self) -> None:
         self.__led.value(1)
-        self.__wlan.active(False)  
+        self.__wlan.active(False)
         time.sleep(1)
         self.__wlan.active(True)
         print("*** Connecting to WiFi...")
 
         try:
             self.__wlan.connect(WIFI_SSID, WIFI_PASS)
-            for i in range(WifiManager.MAX_WAIT_TIME / WifiManager.RECONNECT_INTERVAL):
+            for _ in range(self.MAX_WAIT_TIME / self.RECONNECT_INTERVAL):
                 if self.__wlan.isconnected():
                     print("*** WiFi connected")
                     self.__led.value(0)
                     return
                 time.sleep(WifiManager.RECONNECT_INTERVAL)
-            
+
             print("*** Connection Failed, Retrying...")
         except OSError as e:
             print(f"Error: {e}")
-    
+
     def isconnected(self) -> bool:
         return self.__wlan.isconnected()
 
 
 class MQTTManager:
+    SERVER = "iot.cpe.ku.ac.th"
+    DEBUG_TOPIC = "b6610545499/placeholder/debug"
+
     def __init__(self):
         self.__client = MQTTClient(
-            client_id="",
-            server="iot.cpe.ku.ac.th",
-            user=MQTT_USER,
-            password=MQTT_PASS
+            client_id="", server=self.SERVER, user=MQTT_USER, password=MQTT_PASS
         )
         self.__led = machine.Pin(12, machine.Pin.OUT)
         self.__led.value(1)
@@ -57,12 +56,12 @@ class MQTTManager:
         print("*** Connecting to MQTT broker...")
         try:
             self.__client.connect()
-            self.__client.publish("b6610545499/weather_db/debug", "MQTT Reconnected")
+            self.__client.publish(self.DEBUG_TOPIC, "MQTT Reconnected")
             print("*** MQTT broker connected")
             self.__led.value(0)
         except OSError as e:
             print(f"Error: {e}")
-    
+
     def publish(self, topic: str, payload) -> None:
         try:
             self.__client.publish(topic, payload)
@@ -87,15 +86,16 @@ class MQTTManager:
 
 class ConnectionManager:
     CHECK_CONNECTION_FREQ = 5
+
     def __init__(self):
         self.wifi = WifiManager()
         self.mqtt = MQTTManager()
-    
+
     def connect(self):
         self.wifi.connect()
         if self.wifi.isconnected():
             self.mqtt.connect()
-    
+
     async def check_connection(self):
         """Check WiFi and MQTT connection every 5 seconds"""
         print(f"*** Checking connection every {self.CHECK_CONNECTION_FREQ} seconds")
@@ -106,17 +106,13 @@ class ConnectionManager:
             if self.wifi.isconnected() and not self.mqtt.isconnected():
                 print("*** MQTT disconnected, reconnecting...")
                 self.mqtt.connect()
-#             if not self.wifi.isconnected() or not self.mqtt.isconnected():
-#                 print("*** System unresponsive, restarting...")
-#                 await asyncio.sleep(2)
-#                 machine.reset()
             await asyncio.sleep(self.CHECK_CONNECTION_FREQ)
 
 
 class LightSensor:
     def __init__(self):
         self.__sensor = machine.ADC(machine.Pin(36))
-    
+
     def get_data(self) -> dict:
         v = self.__sensor.read_uv() / 1e06
         r_ldr = v * (33000 / (3.3 - v))
@@ -144,19 +140,19 @@ class Location:
 
 class Publisher:
     PUBLISH_INTERVAL = 600
-    PUBLISH_TOPIC = "b6610545499/weather_db/in"
-    
+    PUBLISH_TOPIC = "b6610545499/placeholder/in"
+
     def __init__(self, *sensors):
         self.conn_mgr = ConnectionManager()
         self.sensors = list(sensors)
         self.conn_mgr.connect()
-    
+
     def __get_all_sensor_data(self) -> dict:
         temp = dict()
         for sensor in self.sensors:
             temp.update(sensor.get_data())
         return temp
-    
+
     async def __publish_sensor_data_every_interval(self):
         while True:
             if self.conn_mgr.wifi.isconnected():
@@ -169,7 +165,10 @@ class Publisher:
         asyncio.create_task(self.conn_mgr.check_connection())
         asyncio.create_task(self.__publish_sensor_data_every_interval())
         asyncio.create_task(self.conn_mgr.mqtt.check_msg())
-        self.conn_mgr.mqtt.publish("b6610545499/weather_db/debug", "KidBright restarted")
+        self.conn_mgr.mqtt.publish(
+            "b6610545499/weather_db/debug", "KidBright restarted"
+        )
+
 
 async def main():
     p = Publisher(LightSensor(), TemperatureSensor(), Location)
@@ -177,9 +176,8 @@ async def main():
     while True:
         await asyncio.sleep(1)
 
+
 try:
     asyncio.run(main())
 except Exception:
     machine.reset()
-
-
