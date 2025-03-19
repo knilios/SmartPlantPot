264e6faa58c7c7b51e1f27007d7d5dbb9a8b3683
Rattapoom Pethputong <celica2548@gmail.com>
2025-03-19 11:27:48 +0700

    Apply Information Expert

--- soil.py
+++ soil.py
@@ -7,6 +7,35 @@ import network
 import time
 
 
+class LightSensor:
+    def __init__(self):
+        self.__sensor = machine.ADC(machine.Pin(36))
+
+    def get_data(self) -> dict:
+        v = self.__sensor.read_uv() / 1e06
+        r_ldr = v * (33000 / (3.3 - v))
+        lux = 10000 / ((r_ldr / 100) ** (4 / 3))
+        return {"light": lux}
+
+
+class TemperatureSensor:
+    def __init__(self):
+        self.__sensor = machine.I2C(sda=machine.Pin(4), scl=machine.Pin(5))
+        self.__sensor.writeto(77, bytearray([0x04, 0b01100000]))
+        self.__sensor.writeto(77, bytearray([0]))
+
+    def get_data(self) -> dict:
+        high, low = self.__sensor.readfrom(77, 2)
+        celsius = (low + (high * 256)) / 128
+        return {"temperature": celsius}
+
+
+class Location:
+    @staticmethod
+    def get_data():
+        return {"lat": 13.45, "lon": 100.29}
+
+
 class WifiManager:
     MAX_WAIT_TIME = 30
     RECONNECT_INTERVAL = 0.5
@@ -80,21 +109,24 @@ class MQTTManager:
         try:
             self.__client.ping()
             return True
-        except:
+        except Exception:
             return False
 
 
-class ConnectionManager:
+class ConnectionController:
     CHECK_CONNECTION_FREQ = 5
 
     def __init__(self):
         self.wifi = WifiManager()
         self.mqtt = MQTTManager()
 
-    def connect(self):
+    def initialise_connection(self):
         self.wifi.connect()
         if self.wifi.isconnected():
             self.mqtt.connect()
+        asyncio.create_task(self.check_connection())
+        asyncio.create_task(self.mqtt.check_msg())
+        self.mqtt.publish(self.mqtt.DEBUG_TOPIC, "KidBright restarted")
 
     async def check_connection(self):
         """Check WiFi and MQTT connection every 5 seconds"""
@@ -109,43 +141,16 @@ class ConnectionManager:
             await asyncio.sleep(self.CHECK_CONNECTION_FREQ)
 
 
-class LightSensor:
-    def __init__(self):
-        self.__sensor = machine.ADC(machine.Pin(36))
-
-    def get_data(self) -> dict:
-        v = self.__sensor.read_uv() / 1e06
-        r_ldr = v * (33000 / (3.3 - v))
-        lux = 10000 / ((r_ldr / 100) ** (4 / 3))
-        return {"light": lux}
-
-
-class TemperatureSensor:
-    def __init__(self):
-        self.__sensor = machine.I2C(sda=machine.Pin(4), scl=machine.Pin(5))
-        self.__sensor.writeto(77, bytearray([0x04, 0b01100000]))
-        self.__sensor.writeto(77, bytearray([0]))
-
-    def get_data(self) -> dict:
-        high, low = self.__sensor.readfrom(77, 2)
-        celsius = (low + (high * 256)) / 128
-        return {"temperature": celsius}
-
-
-class Location:
-    @staticmethod
-    def get_data():
-        return {"lat": 13.45, "lon": 100.29}
-
-
 class Publisher:
     PUBLISH_INTERVAL = 600
     PUBLISH_TOPIC = "b6610545499/placeholder/in"
 
-    def __init__(self, *sensors):
-        self.conn_mgr = ConnectionManager()
+    def __init__(self, *sensors, conn_mgr: ConnectionController):
+        self.conn_mgr = conn_mgr
         self.sensors = list(sensors)
-        self.conn_mgr.connect()
+
+    def run(self):
+        asyncio.create_task(self.__publish_sensor_data_every_interval())
 
     def __get_all_sensor_data(self) -> dict:
         temp = dict()
@@ -161,17 +166,11 @@ class Publisher:
                 self.conn_mgr.mqtt.publish(Publisher.PUBLISH_TOPIC, json.dumps(data))
             await asyncio.sleep(Publisher.PUBLISH_INTERVAL)
 
-    def run(self):
-        asyncio.create_task(self.conn_mgr.check_connection())
-        asyncio.create_task(self.__publish_sensor_data_every_interval())
-        asyncio.create_task(self.conn_mgr.mqtt.check_msg())
-        self.conn_mgr.mqtt.publish(
-            "b6610545499/weather_db/debug", "KidBright restarted"
-        )
-
 
 async def main():
-    p = Publisher(LightSensor(), TemperatureSensor(), Location)
+    conn_mgr = ConnectionController()
+    conn_mgr.initialise_connection()
+    p = Publisher(LightSensor(), TemperatureSensor(), Location, conn_mgr=conn_mgr)
     p.run()
     while True:
         await asyncio.sleep(1)
